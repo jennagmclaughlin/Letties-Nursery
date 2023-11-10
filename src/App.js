ec4722e308855ab2ca48c8a7511716d950e05811
jengmcla <89791791+jengmcla@users.noreply.github.com>
2023-11-10 02:15:58 -0500

    Homepage url

--- src/App.js
+++ src/App.js
@@ -24,16 +24,3 @@ const App = () => {
 };
 
 export default App;
-
-// import "./App.css";
-// import { Navbar } from "./Components/Navbar/Navbar";
-
-// function App() {
-//     return (
-//         <div>
-//             <Navbar />
-//         </div>
-//     );
-// }
-
-// export default App;
